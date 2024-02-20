fcdde0fcd3272f91f71561e5ab116a6a17947a94
Leonardo FAGGIANI <leonardo.faggiani@gruposancristobal.com>
2024-02-20 10:08:51 -0300

    move secret into values

--- backend/Notification/Notifications/Services/EmailSenderService.cs
+++ backend/Notification/Notifications/Services/EmailSenderService.cs
@@ -19,8 +19,8 @@ namespace Notification.Notifications.Services
         public void SendEmail(string content)
         {
             MimeMessage message = this.emailBuilder
-                .SetFrom(configuration.GetSection("EmailSettings:From").Value)
-                .SetTo(configuration.GetSection("EmailSettings:To").Value)
+                .SetFrom(configuration.GetSection("EmailSettingsFrom").Value)
+                .SetTo(configuration.GetSection("EmailSettingsTo").Value)
                 .SetSubject("Contact from MyPage")
                 .SetContent(content)
                 .Build();
@@ -29,7 +29,7 @@ namespace Notification.Notifications.Services
             {
                 client.Connect("smtp-mail.outlook.com", 587, false);
 
-                client.Authenticate(configuration.GetSection("EmailSettings:EmailAuth").Value, configuration.GetSection("EmailSettings:AppPassword").Value);
+                client.Authenticate(configuration.GetSection("EmailSettingsEmailAuth").Value, configuration.GetSection("EmailSettingsAppPassword").Value);
 
                 client.Send(message);
 
